3172fb8bc5e5d295fab7a9138ee34c85eef09603
Lin Junhui <297876621@qq.com>
2025-11-06 14:00:12 +0800

    Delete C++学习/01-基础语法/basic_proj/build/_deps directory

--- C++学习/01-基础语法/basic_proj/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jonson/cuda-learning/C++学习/01-基础语法/basic_proj/build/_deps/googletest-src/googletest
-# Build directory: /home/jonson/cuda-learning/C++学习/01-基础语法/basic_proj/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
